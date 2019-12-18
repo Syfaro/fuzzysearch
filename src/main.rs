97246f502ad0effe01cb1e7f88598efd550ac794
Syfaro <syfaro@huefox.com>
2019-12-18 02:31:10 -0600

    Make async.

--- src/main.rs
+++ src/main.rs
@@ -71,11 +71,7 @@ fn insert_submission(
         .collect();
 
     let hash = sub.hash.clone();
-
-    let url = match &sub.content {
-        furaffinity_rs::Content::Image(url) => url,
-        furaffinity_rs::Content::Flash(url) => url,
-    };
+    let url = sub.content.url();
 
     client.execute("INSERT INTO submission (id, artist_id, url, filename, hash, rating, posted_at, description, hash1, hash2, hash3, hash4, hash5, hash6, hash7, hash8) VALUES ($1, $2, $3, $4, decode($5, 'base64'), $6, $7, $8, get_byte(decode($5, 'base64'), 0), get_byte(decode($5, 'base64'), 1), get_byte(decode($5, 'base64'), 2), get_byte(decode($5, 'base64'), 3), get_byte(decode($5, 'base64'), 4), get_byte(decode($5, 'base64'), 5), get_byte(decode($5, 'base64'), 6), get_byte(decode($5, 'base64'), 7))", &[
         &sub.id, &artist_id, &url, &sub.filename, &hash, &sub.rating.serialize(), &sub.posted_at, &sub.description,
@@ -84,6 +80,7 @@ fn insert_submission(
     let stmt = client.prepare(
         "INSERT INTO tag_to_post (tag_id, post_id) VALUES ($1, $2) ON CONFLICT DO NOTHING",
     )?;
+
     for tag_id in tag_ids {
         client.execute(&stmt, &[&tag_id, &sub.id])?;
     }
@@ -95,7 +92,8 @@ fn insert_null_submission(client: &mut Client, id: i32) -> Result<u64, postgres:
     client.execute("INSERT INTO SUBMISSION (id) VALUES ($1)", &[&id])
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let (cookie_a, cookie_b) = (
         std::env::var("FA_A").expect("missing fa cookie a"),
         std::env::var("FA_B").expect("missing fa cookie b"),
@@ -115,19 +113,19 @@ fn main() {
     'main: loop {
         let mut client = pool.get().unwrap();
 
-        let latest_id = fa.latest_id().expect("unable to get latest id");
+        let latest_id = fa.latest_id().await.expect("unable to get latest id");
 
         for id in ids_to_check(&mut client, latest_id) {
             'attempt: for attempt in 0..3 {
                 if !has_submission(&mut client, id) {
                     println!("loading submission {}", id);
 
-                    let sub = match fa.get_submission(id) {
+                    let sub = match fa.get_submission(id).await {
                         Ok(sub) => sub,
                         Err(e) => {
                             println!("got error: {:?}, retry {}", e.message, e.retry);
                             if e.retry {
-                                std::thread::sleep(std::time::Duration::from_secs(attempt + 1));
+                                tokio::time::delay_for(std::time::Duration::from_secs(attempt + 1)).await;
                                 continue 'attempt;
                             } else {
                                 println!("unrecoverable, exiting");
@@ -145,7 +143,7 @@ fn main() {
                         }
                     };
 
-                    let sub = match fa.calc_image_hash(sub.clone()) {
+                    let sub = match fa.calc_image_hash(sub.clone()).await {
                         Ok(sub) => sub,
                         Err(e) => {
                             println!("unable to hash image: {:?}", e);
@@ -164,6 +162,6 @@ fn main() {
 
         println!("completed fetch, waiting a minute before loading more");
 
-        std::thread::sleep(std::time::Duration::from_secs(60));
+        tokio::time::delay_for(std::time::Duration::from_secs(60)).await;
     }
 }
